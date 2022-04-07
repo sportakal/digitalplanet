700f0300506ede56a6f7d862d0eb9d00e64347b0
sportakal <portakalsinan@gmail.com>
2022-04-07 23:56:00 +0300

    first commit

--- config/digitalplanet.php
+++ config/digitalplanet.php
@@ -2,15 +2,15 @@
 
 return [
     'options' => [
-        'corporate_code' => env('TRENDYOLEFATURAM_CORPORATE_CODE', 'CORPORATE'),
-        'login_name' => env('TRENDYOLEFATURAM_LOGIN_NAME', 'wsadmintest'),
-        'password' => env('TRENDYOLEFATURAM_PASSWORD', 'password'),
-        'url' => env('TRENDYOLEFATURAM_URL', 'https://trendyolintegrationservicewithoutmtomtest.digitalplanet.com.tr/IntegrationService.asmx'),
+        'corporate_code' => env('DIGITALPLANET_CORPORATE_CODE', 'CORPORATE'),
+        'login_name' => env('DIGITALPLANET_LOGIN_NAME', 'wsadmintest'),
+        'password' => env('DIGITALPLANET_PASSWORD', 'password'),
+        'url' => env('DIGITALPLANET_URL', 'https://trendyolintegrationservicewithoutmtomtest.digitalplanet.com.tr/IntegrationService.asmx'),
     ],
 
     'template_codes' => [
-        'earchive' => env('TRENDYOLEFATURAM_EARCHIVE_TEMPLATE_CODE', 'EARCHIVE'),
-        'efatura' => env('TRENDYOLEFATURAM_EFATURA_TEMPLATE_CODE', 'MANUAL'),
+        'earchive' => env('DIGITALPLANET_EARCHIVE_TEMPLATE_CODE', 'EARCHIVE'),
+        'efatura' => env('DIGITALPLANET_EFATURA_TEMPLATE_CODE', 'MANUAL'),
     ],
 
     'sender' => [
