4b2907bb52d3b81674d95373207f70a8fc21893b
sportakal <portakalsinan@gmail.com>
2024-01-08 16:51:27 +0300

    feat(requests): add CancelEArchiveInvoiceRequest (main)
    Add CancelEArchiveInvoiceRequest class to handle canceling e-archive invoices.
    The class includes properties for Value, Type, and CancelDate. The Value property represents the value of the given type in the ValuType field. The Type property represents the type of parameter to be obtained for the invoice. The CancelDate property represents the date on which the cancellation is made.
    
    Issue: main

---
+++ src/Requests/CancelEArchiveInvoiceRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sportakal\Digitalplanet\Requests;
+
+
+use Carbon\Carbon;
+
+class CancelEArchiveInvoiceRequest extends DigitalplanetRequest
+{
+
+    protected string $Value; //ValuType alanında hangi tip verilmiş ise o tipe ait değer girilir. Örneğin Type olarak “INVOICEID” girilmiş ise, “GIB2014000000001” girilebilir.
+    protected string $Type; //Faturanın alınacağı parametrenin tipidir. Alabileceği değerler “UUID” , “INVOICEID” ve “REFID” dir. UUID faturanın GUID numarasını temsil eder. INVOICEID faturanın 16 haneli gib numarasını temsil eder. REFID ise gib numarasını müşterilerimiz için Digital Planet’in üretmesi durumunda, müşterilerimizin bize her fatura için ayrı değer olarak gönderdikleri referans numarasını temsil eder.
+//    protected string $TotalAmount;
+    protected string $CancelDate;
+
+    public function __construct(string $Value,
+                                       $Type = 'INVOICEID',
+                                       $CancelDate = null
+    )
+    {
+        if ($CancelDate == null) {
+            $CancelDate = Carbon::now()->format('Y-m-d');
+        }
+
+        $this->Value = $Value;
+        $this->Type = $Type;
+//        $this->TotalAmount = $TotalAmount;
+        $this->CancelDate = $CancelDate;
+    }
+}
+
